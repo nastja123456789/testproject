1db504579538685dcb0bda88f641e4e46b60103a
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2024-11-23 20:53:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ db_config = {
 @app.route('/number', methods=['POST'])
 def process_number():
     if not request.json or 'number' not in request.json:
-        return jsonify({'error': 'Bad request: No number provided.'}), 400
+        return jsonify({'error': 'Bad request: Число не указано.'}), 400
 
     incoming_number = request.json['number']
 
     if not isinstance(incoming_number, int) or incoming_number < 0:
-        return jsonify({'error': 'The number must be a natural number from 0 to N.'}), 400
+        return jsonify({'error': 'Неправильный формат ввода числа - число не от 0 до N.'}), 400
 
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
@@ -33,7 +33,7 @@ def process_number():
         # Проверка на уже поступившие числа
         cursor.execute("SELECT number FROM numbers WHERE number = %s", (incoming_number,))
         if cursor.fetchone() is not None:
-            error_message = f"Error: The number {incoming_number} has already been processed."
+            error_message = f"Ошибка: Число {incoming_number} уже было отправлено."
             logging.error(error_message)
             return jsonify({'error': error_message}), 400
 
@@ -42,7 +42,7 @@ def process_number():
         last_number = cursor.fetchone()
 
         if last_number is not None and incoming_number < last_number[0]:
-            error_message = f"Error: The number {incoming_number} is less than the last processed number {last_number[0]}."
+            error_message = f"Ошибка: Число {incoming_number} меньше чем последнее отправленное число {last_number[0]}."
             logging.error(error_message)
             return jsonify({'error': error_message}), 400
 
@@ -55,7 +55,7 @@ def process_number():
 
     except Exception as e:
         logging.error(f"Database error: {str(e)}")
-        return jsonify({'error': 'Could not process the number.'}), 500
+        return jsonify({'error': 'Не может обработать число.'}), 500
 
     finally:
         cursor.close()
